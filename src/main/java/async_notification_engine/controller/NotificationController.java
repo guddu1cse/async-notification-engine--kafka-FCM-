ae624da4938347baf74cd00b5f4907ab34759926
Guddu_Kumar <guddu1cse@gmail.com>
2026-02-13 04:07:37 +0530

    docker setup

--- src/main/java/async_notification_engine/controller/NotificationController.java
+++ src/main/java/async_notification_engine/controller/NotificationController.java
@@ -5,6 +5,7 @@ import async_notification_engine.repository.NotificationRepository;
 import async_notification_engine.service.FcmService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,6 +23,11 @@ public class NotificationController {
     public NotificationController(FcmService fcmService, NotificationRepository notificationRepository) {
         this.fcmService = fcmService;
         this.notificationRepository = notificationRepository;
+    }
+
+    @GetMapping("/health")
+    public Map<String, String> health() {
+        return Map.of("status", "UP", "service", "notification-engine");
     }
 
     @PostMapping("/test")
